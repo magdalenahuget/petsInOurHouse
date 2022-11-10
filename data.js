bf30d28c6229383995e3cb6127fbbae43d2513fd
magdalenahuget <magdalena.huget@gmail.com>
2022-11-10 18:26:44 +0100

    Average age and the youngest animal in our house

---
+++ data.js
@@ -0,0 +1,413 @@
+let petsInOurHouse = {
+    name: "Pets in our house",
+    members: [
+        {
+            name: "Cat Marcel",
+            birthday: {
+                year: "2015",
+                month: "July",
+                day: "27",
+            },
+            location: {
+                country: "Poland",
+                city: "Koszalin",
+            },
+            codingLevel: 25,
+            favourites: {
+                favSnacks: [
+                    {
+                        title: "Callibra",
+                        rating: 8.8,
+                        producerCountry: ["Poland"],
+                        genres: ["dry food", "package"],
+                        review: [
+                            "This is my favorite dry meal.",
+                        ],
+                    },
+                    {
+                        title: "Whiskas",
+                        rating: 6,
+                        producerCountry: ["France"],
+                        genres: ["wet food", "sachet"],
+                        review: [
+                            "This is my favorite wet meal.",
+                        ],
+                    },
+                    {
+                        title: "Royal Canin",
+                        rating: 6,
+                        producerCountry: ["France"],
+                        genres: ["wet food", "can"],
+                        review: [
+                            "This is my favorite wet meal in can.",
+                        ],
+                    },
+                ],
+                favFun: [
+                    {
+                        funName: "sleeping",
+                    },
+                    {
+                        funName: "eating",
+                    },
+                    {
+                        funName: "meowing",
+                    },
+                ],
+                favSongs: [
+                    {
+                        title: "99 Problems",
+                        artist: "Jay-Z",
+                        genres: ["rap rock", "hip-hop"],
+                        review:
+                            "He is my favorite artist and this song made me listen to him.",
+                    },
+                    {
+                        title: "Lost On You",
+                        artist: "LP",
+                        genres: ["indie rock", "folk rock"],
+                        review:
+                            "I have many memories with this song and it has a lot of emotions and thoughts in me.",
+                    },
+                    {
+                        title: "Way Down We Go",
+                        artist: "Kaleo",
+                        genres: ["blues rock"],
+                        review:
+                            "I love the lyrics to this song, and when I listen to it I just focus on it. I like to listen to it the most when I am driving my car at night.",
+                    },
+                ],
+            },
+            colours: {
+                primary: {
+                    red: 52,
+                    green: 185,
+                    blue: 184
+                },
+                secondary: {
+                    red: 50,
+                    green: 50,
+                    blue: 50
+                },
+            },
+        },
+        {
+            name: "Goat Bunia",
+            birthday: {
+                year: "2005",
+                month: "April",
+                day: "31",
+            },
+            location: {
+                country: "Poland",
+                city: "Leszno",
+            },
+            codingLevel: 20,
+            favourites: {
+                favSnacks: [
+                    {
+                        title: "Farmer",
+                        rating: 9.6,
+                        producerCountry: ["Poland"],
+                        genres: ["dry food", "package"],
+                        review: [
+                            "This is my favorite dry meal.",
+                        ],
+                    },
+                    {
+                        title: "Unipasz",
+                        rating: 7,
+                        producerCountry: ["Germany"],
+                        genres: ["dry food", "sachet"],
+                        review: [
+                            "This is my favorite wet meal.",
+                        ],
+                    },
+                    {
+                        title: "Kozinex",
+                        rating: 6,
+                        producerCountry: ["Poland"],
+                        genres: ["dry food", "can"],
+                        review: [
+                            "This is my favorite wet meal in can.",
+                        ],
+                    },
+                ],
+                favFun: [
+                    {
+                        funName: "jumping",
+                    },
+                    {
+                        funName: "eating",
+                    },
+                ],
+                favSongs: [
+                    {
+                        title: "Halo",
+                        artist: "Beyonce",
+                        genres: ["pop"],
+                        review:
+                            "Halo is my favourite song, but I don't know why. I can listen it all the time and never get bored.",
+                    },
+                    {
+                        title: "Lovely",
+                        artist: "Billie Eilish",
+                        genres: ["pop"],
+                        review:
+                            "Lovely is very good song with great duo singing Billie Eilish and Khalid. I like this song, because I saw a lot of tiktoks with it.",
+                    },
+                    {
+                        title: "The Real Slim Shady",
+                        artist: "Eminem",
+                        genres: ["hip-hop"],
+                        review:
+                            "The Real Slim Shady is different from the rest of my favourite songs, but I really like Eminem's flow and text of this song.",
+                    },
+                ],
+            },
+            colours: {
+                primary: {
+                    red: 242,
+                    green: 121,
+                    blue: 0
+                },
+                secondary: {
+                    red: 50,
+                    green: 50,
+                    blue: 50
+                },
+            },
+        },
+        {
+            name: "Iguana Robert",
+            birthday: {
+                year: "2016",
+                month: "November",
+                day: "1",
+            },
+            location: {
+                country: "Poland",
+                city: "Katowice",
+            },
+            codingLevel: 15,
+            favourites: {
+                favSnacks: [
+                    {
+                        title: "Iguvert",
+                        rating: 9.6,
+                        producerCountry: ["Poland"],
+                        genres: ["dry food", "package"],
+                        review: [
+                            "This is my favorite dry meal.",
+                        ],
+                    },
+                    {
+                        title: "Reptiles",
+                        rating: 7,
+                        producerCountry: ["Turkey"],
+                        genres: ["dry food", "sachet"],
+                        review: [
+                            "This is my favorite wet meal.",
+                        ],
+                    },
+                    {
+                        title: "Salates",
+                        rating: 6,
+                        producerCountry: ["Poland"],
+                        genres: ["fresh food", "packet"],
+                        review: [
+                            "This is my favorite wet meal in can.",
+                        ],
+                    },
+                ],
+                favFun: [
+                    {
+                        funName: "standing",
+                    },
+                    {
+                        funName: "eating",
+                    },
+                    {
+                        funName: "observing",
+                    },
+                ],
+                favSongs: [
+                    {
+                        title: "That's What I Like",
+                        artist: "Bruno Mars",
+                        genres: ["hip-hop", "soul"],
+                        review: "A funky song to stamp the foot to the beat.",
+                    },
+                    {
+                        title: "I see fire",
+                        artist: "Ed Sheeran",
+                        genres: ["folk", "pop"],
+                        review: "A nice quiet melody for autumn evenings.",
+                    },
+                    {
+                        title: "Explosion",
+                        artist: "Kalwi&Remi",
+                        genres: ["trans", "dance"],
+                        review: "Great song you can dance to it. It gives energy.",
+                    },
+                ],
+            },
+            colours: {
+                primary:
+                {
+                    red: 255,
+                    green: 128,
+                    blue: 204
+                },
+                secondary:
+                {
+                    red: 0,
+                    green: 115,
+                    blue: 191
+                },
+            },
+        },
+        {
+            name: "Horse Rafal",
+            birthday: {
+                year: "1995",
+                month: "May",
+                day: "22",
+            },
+            location: {
+                country: "Poland",
+                city: "Zakopane",
+            },
+            codingLevel: 10,
+
+            favourites: {
+                favSnacks: [
+                    {
+                        title: "Farmer",
+                        rating: 9.6,
+                        producerCountry: ["Poland"],
+                        genres: ["dry food", "package"],
+                        review: [
+                            "This is my favorite dry meal.",
+                        ],
+                    },
+                    {
+                        title: "Carrotix",
+                        rating: 8,
+                        producerCountry: ["Italy"],
+                        genres: ["dry food", "sachet"],
+                        review: [
+                            "This is my favorite wet meal.",
+                        ],
+                    },
+                    {
+                        title: "Applex",
+                        rating: 7,
+                        producerCountry: ["Portugal"],
+                        genres: ["dry food", "can"],
+                        review: [
+                            "This is my favorite wet meal in can.",
+                        ],
+                    },
+                ],
+                favFun: [
+                    {
+                        funName: "jumping",
+                    },
+                    {
+                        funName: "eating",
+                    },
+                    {
+                        funName: "galloping",
+                    },
+                ],
+                favSongs: [
+                    {
+                        title: "La Bachata",
+                        artist: "Manuel Turizo",
+                        genres: ["pop"],
+                        review: "Why has latin music this year been so good.",
+                    },
+                    {
+                        title: "Under The Influence",
+                        artist: "Chris Brown.",
+                        genres: ["pop"],
+                        review:
+                            "Best songs: Pop A Top, Farewell Party, Right In The Palm Of Your Hand, She Just Started Liking Cheatin' Songs, It Must Be Love.",
+                    },
+                    {
+                        title: "STAR WALKIN",
+                        artist: "Lil Nas X",
+                        genres: ["pop"],
+                        review:
+                            "Not my favorite Lil Nas track ever, but still an enjoyable blend of drill and a throwback pop mix.",
+                    },
+                ],
+            },
+            colours: {
+                primary:
+                {
+                    red: 100,
+                    green: 200,
+                    blue: 200
+                },
+                secondary:
+                {
+                    red: 50,
+                    green: 50,
+                    blue: 50
+                },
+            },
+        },
+    ],
+};
+
+// Average age - for...  of
+
+let sumAverageAge = 0;
+let averageAge = 0;
+let currentYear = 2022;
+let animalAgeIntoString
+for (let animal of petsInOurHouse.members) {
+    animalAgeIntoString = parseInt(animal.birthday.year);
+    sumAverageAge += (currentYear - animalAgeIntoString)
+}
+averageAge = sumAverageAge / petsInOurHouse.members.length
+averageAge = Math.round(averageAge)
+console.log(averageAge)
+
+// Average age - for i
+
+sumAverageAge = 0;
+currentYear = 2022;
+for (i = 0; i < petsInOurHouse.members.length; i++) {
+    animalYearIntoInteger = parseInt(petsInOurHouse.members[i].birthday.year);
+    sumAverageAge += (currentYear - animalYearIntoInteger);
+}
+averageAge = sumAverageAge / petsInOurHouse.members.length
+averageAge = Math.round(averageAge)
+console.log(averageAge)
+
+// The youngest member - for of
+
+let youngestAnimal = petsInOurHouse.members[0];
+for (let animal of petsInOurHouse.members) {
+    if (youngestAnimal.birthday.year < animal.birthday.year) {
+        youngestAnimal = animal
+    }
+}
+youngestAnimal = youngestAnimal.name
+console.log(youngestAnimal)
+
+// The youngest member - for i
+
+youngestAnimal = petsInOurHouse.members[0];
+for (let i = 0; i < petsInOurHouse.members.length; i++) {
+    if (youngestAnimal.birthday.year < petsInOurHouse.members[i].birthday.year) {
+        youngestAnimal = petsInOurHouse.members[i];
+    }
+}
+youngestAnimal = youngestAnimal.name
+console.log(youngestAnimal)
+
+// The oldest member
